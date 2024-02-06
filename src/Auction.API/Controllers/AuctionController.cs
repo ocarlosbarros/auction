40e37c6df4cd383144788c3de338ad741080fb3a
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-06 17:25:10 -0300

    feat(AuctionController):changes GetCurrentAuction method such as return, validation

--- src/Auction.API/Controllers/AuctionController.cs
+++ src/Auction.API/Controllers/AuctionController.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Auction.API.UseCases.Auctions.GetCurrent;
 
 namespace Auction.API.Controllers;
+
+using Entities;
 
 [Route("[controller]")]
 [ApiController]
 public class AuctionController : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult GetCurrentAuction()
     {
-        return Ok("Bem Vindo, NLW Expert!");
+        var useCase = new GetCurrentAuctionUseCase();
+        
+        var response = useCase.Execute();
+        
+        if (response is null)
+            return NoContent();
+        
+        return Ok(response);
     }
 }
