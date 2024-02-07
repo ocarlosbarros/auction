7a54be2ea07ad435c04adfed521735d61fcfd057
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-07 00:11:06 -0300

    feat(AuctionDbContext):adds DbSet to Offers

--- src/Auction.API/Repositories/AuctionDbContext.cs
+++ src/Auction.API/Repositories/AuctionDbContext.cs
@@ -7,6 +7,7 @@ public class AuctionDbContext : DbContext
 {
     public DbSet<Auction> Auctions { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Offer> Offers { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(@"Data Source=/home/ocarlosbarros/Repos/databases/auctionDb.db");
