c0b8e094ca0017652e7e394574966fdcecd8d8a0
Carlos Barros <carlos_dbs@hotmail.com>
2024-02-07 00:15:39 -0300

    feat(OfferController):completes CreateOffer implementation using useCase to return Id of Offer created

--- src/Auction.API/Controllers/OfferController.cs
+++ src/Auction.API/Controllers/OfferController.cs
@@ -1,5 +1,6 @@
 using Auction.API.Communication.Requests;
 using Auction.API.Filters;
+using Auction.API.UseCases.Offers.CreateOffer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Auction.API.Controllers;
@@ -8,8 +9,13 @@ public class OfferController : AuctionBaseController
 {
     [HttpPost]
     [Route("{itemId}")]
-    public IActionResult CreateOffer([FromRoute]int itemId, [FromBody] RequestCreateOfferJson request)
+    public IActionResult CreateOffer(
+        [FromRoute]int itemId, 
+        [FromBody] RequestCreateOfferJson request,
+        [FromServices] CreateOfferUseCase useCase)
     {
-        return Created();
+        var id = useCase.Execute(itemId, request);
+        
+        return Created(string.Empty, id);
     }
 }
